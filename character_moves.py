f5ffb19cee0887d2b94ccbe319eebd355a574a79
SEUNGMIN KIM <231153824+kehne100-pixel@users.noreply.github.com>
2025-09-19 22:57:37 +0900

    삼각 운동 완성

--- character_moves.py
+++ character_moves.py
@@ -3,12 +3,12 @@ from pico2d import *
 open_canvas()
 
 boy = load_image('character.png')
-
+grass = load_image('grass.png')
 
 def move_top():
     print('Moving top')
     for x in range(0, 800, 30):
-         draw_boy(x, 550)
+     draw_boy(x, 550)
     pass
 
 
@@ -16,7 +16,7 @@ def move_right():
     print('Moving right')
     for y in range(550, 50, -30):
         draw_boy(780, y)
-    pass
+        pass
 
 
 def move_bottom():
@@ -26,11 +26,13 @@ def move_bottom():
     pass
 
 
+
 def move_left():
     print('Moving left')
     for y in range(100, 550, 30):
         draw_boy(20, y)
     pass
+
 
 
 def move_rectangle():
@@ -40,7 +42,7 @@ def move_rectangle():
     move_bottom()
     move_left()
 
-    pass
+
 
 
 def move_circle():
@@ -49,21 +51,53 @@ def move_circle():
     for deg in range(0, 360):
         x =  r * math.cos(math.radians(deg)) + 400
         y =  r * math.sin(math.radians(deg)) + 300
-
         draw_boy(x, y)
-    pass
+
 
 
 def draw_boy(x: float, y: float):
     clear_canvas_now()
     boy.draw_now(x, y)
+    grass.draw_now(400, 30)
     delay(0.1)
 
+def move_up_diagonal():
+   for x in range(0, 400, 5):
+         y = x + 50
+         draw_boy(x, y)
+         pass
+
+
+
+
+def move_down_diagonal():
+
+    for x in range(400, 800, 5):
+        y= 850 - x
+        draw_boy(x, y)
+        pass
+
+
+
+
+
+def move_triangle():
+
+    print("move triangle")
+    move_up_diagonal()
+    move_down_diagonal()
+    move_bottom()
+
+
+
+    pass
 
 while True:
     # move_circle()
-    move_rectangle()
-    break
+    #move_rectangle()
+    move_triangle()
+
+
 
     pass
 
